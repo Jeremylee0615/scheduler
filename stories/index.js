bb01ad6461d9a51e9ed78644b37433f8f7da8e4d
Jeremy Lee <jeremylee0615@gmail.com>
2023-07-28 17:05:48 -0700

    create initial set up for Appointment components

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,10 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment/Header";
+import Appointment from "components/Appointment/Empty";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -134,3 +138,10 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
